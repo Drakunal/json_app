a703025be9717bfb1fc996b6b3a89af49dfd6b4b
Drakunal <drakunaldey1@gmail.com>
2021-09-28 20:10:06 +0530

    future builder implemented

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,16 +17,27 @@ class _HomeState extends State<Home> {
     data =
         Network(url: "https://jsonplaceholder.typicode.com/posts").fetchData();
     data!.then((value) {
-      print(value[0]);
+      // print(value[0]);//for the whole json object
+      print(value[0]['title']); //for a key of json object
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Json"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Json"),
+        ),
+        body: Center(
+          child: FutureBuilder(
+              future: data,
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data[0]['title']);
+                } else {
+                  return CircularProgressIndicator();
+                }
+              }),
+        ));
   }
 }
